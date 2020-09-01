e71d83e8c94fbc3f0dbcc35d8a1cce7668d8a3fc
renan <renancunhafonseca@gmail.com>
2020-09-01 16:36:40 -0300

    Add data dir

---
+++ data/get_data.sh
@@ -0,0 +1,3 @@
+# enade data
+curl http://download.inep.gov.br/microdados/Enade_Microdados/microdados_Enade_2017_portal_2018.10.09.zip --output microdados_Enade_2017_portal_2018.10.09.zip
+unzip -d enade microdados_Enade_2017_portal_2018.10.09.zip
