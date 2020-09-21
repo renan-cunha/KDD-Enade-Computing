f2e3c822dbc5f3565d55bf02fd9a8efbd796e16a
renan <renancunhafonseca@gmail.com>
2020-09-21 11:41:27 -0300

    Add test_objective_scores function

--- test/test_process_enade.py
+++ test/test_process_enade.py
@@ -33,15 +33,20 @@ data = [
 ]
 
 
+def auxiliar_function_scores(input: str, expected: str) -> tuple:
+    input_df = pd.read_csv(StringIO(input), dtype={"DS_VT_ACE_OCE": str})
+    expected_df = pd.read_csv(StringIO(expected))
+    expected_df = pd.concat([input_df.copy(), expected_df], axis=1)
+    process_2017 = ProcessEnade2014_2017(2017)
+    return process_2017, expected_df, input_df
+
+
 @pytest.mark.parametrize("input, expected, key, gen_ids, gen_labels, "
                          "spe_ids, spe_labels", data)
 def test_discursive_scores(input: str, expected: str, key: bool, gen_ids: list,
                            gen_labels: list, spe_ids: list,
                            spe_labels: list) -> None:
-    input_df = pd.read_csv(StringIO(input))
-    expected_df = pd.read_csv(StringIO(expected))
-    expected_df = pd.concat([input_df.copy(), expected_df], axis=1)
-    process_2017 = ProcessEnade2014_2017(2017)
+    process_2017, expected_df, input_df = auxiliar_function_scores(input, expected)
     process_2017.GEN_DIS_QUESTIONS_LABEL = gen_labels
     process_2017.GEN_DIS_QUESTIONS_ID = gen_ids
     process_2017.SPE_DIS_QUESTIONS_LABEL = spe_labels
@@ -51,3 +56,35 @@ def test_discursive_scores(input: str, expected: str, key: bool, gen_ids: list,
     assert output_df.equals(expected_df)
 
 
+input_csv_objective_columns = "DS_VT_ACE_OFG,DS_VT_ACE_OCE,DS_VT_ESC_OFG," \
+                           "DS_VT_ESC_OCE\n" \
+
+objective_data = [
+    (input_csv_objective_columns + "900019,10009,A.*BC.,E.*AB",
+     ",".join([f"QUESTAO_{x}_NOTA,QUESTAO_{x}_STATUS" for x in range(1, 7)]) + "\n"
+     "NULA,NULA,BRANCO,OK,RASURA,OK,0,OK,100,OK,BRANCO,NULA",
+     True, [1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5], [7, 8, 9, 10, 11], [0, 1, 2, 3, 4]),
+    (input_csv_objective_columns + "01,0001,AB,..AB",
+     ",".join(
+         [f"QUESTAO_{x}_NOTA,QUESTAO_{x}_STATUS" for x in [11, 13]]) + "\n"
+        "0,OK,100,OK",
+     False, [1, 2], [0, 1], [11, 13],
+     [2, 3]),
+]
+
+
+@pytest.mark.parametrize("input, expected, key, gen_ids, gen_labels, "
+                         "spe_ids, spe_labels", objective_data)
+def test_objective_scores(input: str, expected: str, key: bool, gen_ids: list,
+                           gen_labels: list, spe_ids: list,
+                           spe_labels: list) -> None:
+    process_2017, expected_df, input_df = auxiliar_function_scores(input, expected)
+    process_2017.GEN_OBJ_QUESTIONS_LABEL = gen_labels
+    process_2017.GEN_OBJ_QUESTIONS_ID = gen_ids
+    process_2017.SPE_OBJ_QUESTIONS_LABEL = spe_labels
+    process_2017.SPE_OBJ_QUESTIONS_ID = spe_ids
+    expected_df = expected_df.astype(str)
+    input_df = input_df.astype(str)
+    output_df = process_2017.get_objective_scores(input_df, key)
+    output_df = output_df.astype(str)
+    assert output_df.equals(expected_df)
