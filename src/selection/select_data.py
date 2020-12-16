4f174cd42ef130a822e05f1784375d75ea30beef
renan <renancunhafonseca@gmail.com>
2020-12-16 12:15:05 -0300

    Add filter_computer_science_2005 function

--- src/selection/select_data.py
+++ src/selection/select_data.py
@@ -1,8 +1,11 @@
 import pandas as pd
+from typing import Tuple
+from src import config
+
 
 COMPUTER_SCIENCE_CODE_2017_2014_2011 = 4004
 COMPUTER_SCIENCE_CODE_2008 = 4001
-COMPUTER_SCIENCE_CODE_2005 = 40
+COMPUTER_CODE_2005 = 40
 
 
 def filter_computer_science_2017_2014_2011(df: pd.DataFrame) -> pd.DataFrame:
@@ -12,13 +15,63 @@ def filter_computer_science_2017_2014_2011(df: pd.DataFrame) -> pd.DataFrame:
 def filter_computer_science_2008(df: pd.DataFrame) -> pd.DataFrame:
     return df.loc[df["co_subarea"] == COMPUTER_SCIENCE_CODE_2008]
 
-"""
 
-def filter_computer_science_2005(df: pd.DataFrame) -> pd.Dataframe:
+def has_same_value(series: pd.Series) -> bool:
+    """Returns true if the series has the same value in each row,
+    returns false otherwise."""
+    series_unique = series.unique()
+    length_series = len(series_unique)
+    if length_series > 1:
+        return False
+    elif length_series == 0:
+        raise ValueError("The series is empty")
+    else:
+        return True
 
+
+def get_computer_science_answer_key_2005(ufpa_cc_score_specific: pd.Series) -> Tuple[str, str]:
+    """Returns the regex used to match the 'vt_ace_oce' with computer science
+    courses. It gets that by using the 'vt_ace_oce' of the known UFPA computer
+    science course"""
+
+    length_scores = ufpa_cc_score_specific.str.len()
+    if not has_same_value(length_scores):
+        raise ValueError("The series should have values with the same length")
+
+    def get_num_starting_dots(series: pd.Series, mode: str) -> int:
+        length_value = len(series.iloc[0])
+        num_dots_result = 0
+        for num_dots in range(1, length_value):
+            dot_string = "." * num_dots
+            if mode == "start":
+                equal_to_dot = series.str.startswith(dot_string)
+            elif mode == "end":
+                equal_to_dot = series.str.endswith(dot_string)
+            else:
+                raise ValueError(f"Use 'start' or 'end' as mode not {mode}")
+            if equal_to_dot.all():
+                num_dots_result = num_dots
+            else:
+                break
+        return num_dots_result
+
+    num_starting_dots = get_num_starting_dots(ufpa_cc_score_specific, 'start')
+    num_ending_dots = get_num_starting_dots(ufpa_cc_score_specific, 'end')
+    return '.' * num_starting_dots, '.' * num_ending_dots
+
+
+def select_ufpa_computer_science_2005(df: pd.DataFrame) -> pd.DataFrame:
+    return df.loc[df["co_curso"] == config.CODE_UFPA_COURSE]
+
+
+def filter_specific_score_2005(df: pd.DataFrame) -> pd.DataFrame:
+    return df["vt_ace_oce"]
+
+
+def filter_computer_science_2005(df: pd.DataFrame) -> pd.DataFrame:
+
+    """
     def is_computer_science(score_questions: str) -> bool:
-        "#Returns true if the right answers are the same as the one from
-        computer science"
         start = "." * 28
         end = "." * 14
         if type(score_questions) == float:
@@ -27,10 +80,15 @@ def filter_computer_science_2005(df: pd.DataFrame) -> pd.Dataframe:
             # if it has at least one answer different than blank
             return True
         else:
-            return False
+            return False"""
 
-    df = df.loc[df["co_grupo"] == COMPUTER_SCIENCE_CODE_2005]
-    boolean_index = df["vt_ace_oce"].apply(is_computer_science)
-    return df.loc[boolean_index]
+    ufpa_comp_sci = select_ufpa_computer_science_2005(df)
+    ufpa_comp_sci_specific_score = filter_specific_score_2005(ufpa_comp_sci)
+    computer_science_dot_match = get_computer_science_answer_key_2005(ufpa_comp_sci_specific_score)
 
-"""
+    starting_dots, ending_dots = computer_science_dot_match
+
+    computer_df = df.loc[df["co_grupo"] == COMPUTER_CODE_2005]
+    starting_dot_index = computer_df["vt_ace_oce"].str.startswith(starting_dots)
+    ending_dot_index = computer_df["vt_ace_oce"].str.endswith(ending_dots)
+    return computer_df.loc[starting_dot_index & ending_dot_index]
--- tests/test_selection/test_select_data.py
+++ tests/test_selection/test_select_data.py
@@ -1,6 +1,7 @@
 import pytest
 from src.selection import select_data
 import pandas as pd
+from src import config
 
 
 class TestFilter2017and2014and2011:
@@ -20,6 +21,7 @@ class TestFilter2017and2014and2011:
 
         assert output_df.equals(expected_df)
 
+
 class TestFilter2008:
 
     def test_data_frame(self):
@@ -35,4 +37,46 @@ class TestFilter2008:
 
         output_df = select_data.filter_computer_science_2008(input_df)
 
+        assert output_df.equals(expected_df)
+
+
+class TestGetComputerScienceAnswerKey2005:
+
+    def test_raise_value_error(selfs) -> None:
+
+        input_series = pd.Series(["...", ".."])
+        with pytest.raises(ValueError):
+            select_data.get_computer_science_answer_key_2005(input_series)
+
+    def test_example(self) -> None:
+        input_series = pd.Series([".ad..", "..d.."])
+
+        expected_output = (".", "..")
+
+        output = select_data.get_computer_science_answer_key_2005(input_series)
+
+        assert output == expected_output
+
+
+class TestFilter2005:
+
+    def test_example(self) -> None:
+
+        input_dataframe = pd.DataFrame({"co_curso": [config.CODE_UFPA_COURSE,
+                                                     config.CODE_UFPA_COURSE,
+                                                     1245, 1256, 1257],
+                                        "vt_ace_oce": [".abc..", ".ab...",
+                                                       ".acc..",
+                                                       ".adec.",
+                                                       ".abc.."],
+                                        "co_grupo": [40, 40, 40, 40, 12]})
+
+        expected_df = pd.DataFrame({"co_curso": [config.CODE_UFPA_COURSE,
+                                                 config.CODE_UFPA_COURSE,
+                                                 1245],
+                                    "vt_ace_oce": [".abc..", ".ab...",
+                                                  ".acc.."],
+                                    "co_grupo": [40, 40, 40]})
+
+        output_df = select_data.filter_computer_science_2005(input_dataframe)
         assert output_df.equals(expected_df)
