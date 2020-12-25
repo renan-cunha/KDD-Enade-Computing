14a608b3378274863df6269ff0ffc90b4aeb4d71
renan-cunha <renancunhafonseca@gmail.com>
2020-12-25 15:34:06 -0300

    Add pre_processing package

---
+++ src/pre_processing/pre_process.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+
+import pandas as pd
+
+import sys
+parent = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')) #this should give you absolute location of my_project folder.
+sys.path.append(parent)
+from src import config
+from src.selection import select_data
+from src.data.get_raw_data import DATA_DIR
+
+
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed_data")
+
+
+def get_processed_csv_file_path(year: int,
+                                path: str = PROCESSED_DATA_DIR) -> str:
+    return os.path.join(path, f"microdados_processado_{year}.csv")
+
+
+def remove_missing_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Filters the students that were present but have missing values"""
+    missing_values = df["DS_VT_ESC_OCE"].isna()
+    present = df["TP_PRES"] == config.STUDENT_CODE_PRESENT
+    boolean_index = ~(missing_values & present)
+    return df.loc[boolean_index]
+
+
+def rename_columns_2008_2005(df: pd.DataFrame) -> pd.DataFrame:
+    """Rename columns of enade dataframes of years 2005 in 2008
+    The new name is the same as the datasets from more recent years"""
+    df.columns = [x.upper() for x in df.columns]
+    columns_to_rename = ["VT_ESC_OFG", "VT_ESC_OCE", "VT_ACE_OCE", "VT_ACE_OFG"]
+    new_columns = [f"DS_{x}" for x in columns_to_rename]
+
+    df.rename(columns=dict(zip(columns_to_rename, new_columns)), inplace=True)
+    return df
+
+
+def main(select_path: str = select_data.SELECTED_DATA_DIR,
+         processed_path: str = PROCESSED_DATA_DIR):
+
+    subprocess.run(["mkdir", "-p", processed_path])
+    for year in config.YEARS:
+        df_year = select_data.read_csv(year, select_path)
+        if year in [2008, 2005]:
+            df_year = rename_columns_2008_2005(df_year)
+        df_year_cleaned = remove_missing_data(df_year)
+        file_path = get_processed_csv_file_path(year, processed_path)
+        df_year_cleaned.to_csv(file_path, index=False)
+
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ tests/test_pre_processing/test_pre_process.py
@@ -0,0 +1,93 @@
+import pytest
+from src.pre_processing import pre_process
+import pandas as pd
+import os
+from pytest_mock import MockerFixture
+import subprocess
+
+
+class TestRemoveMissingData:
+
+    def test_remove_missing_data(self) -> None:
+        input_df = pd.DataFrame({"DS_VT_ESC_OCE": ["1344", "3545", None, None],
+                                 "TP_PRES": [555, 222, 555, 222]})
+        expected_df = pd.DataFrame({"DS_VT_ESC_OCE": ["1344", "3545", None],
+                                  "TP_PRES": [555, 222, 222]}, index=[0, 1, 3])
+
+        output_df = pre_process.remove_missing_data(input_df)
+        assert output_df.equals(expected_df)
+
+
+class TestRenameColumns:
+
+    def test_rename_columns(self) -> None:
+        input_df = pd.DataFrame(columns = ["a",
+                                       "vt_ace_oce",
+                                       "vt_ace_ofg",
+                                       "vt_esc_oce",
+                                       "vt_esc_ofg"])
+        expected_df = pd.DataFrame(columns=["A",
+                                            "DS_VT_ACE_OCE",
+                                            "DS_VT_ACE_OFG",
+                                            "DS_VT_ESC_OCE",
+                                            "DS_VT_ESC_OFG"])
+        output_df = pre_process.rename_columns_2008_2005(input_df)
+
+        assert output_df.equals(expected_df)
+
+
+class TestGetFilePath:
+
+    def test_get_file_path(self, tmpdir) -> None:
+
+        expected = os.path.join(tmpdir, "microdados_processado_2005.csv")
+        output = pre_process.get_processed_csv_file_path(2005, tmpdir)
+
+        assert expected == output
+
+
+class TestMain:
+
+    def side_effect(self, input: pd.DataFrame) -> pd.DataFrame:
+        return input
+
+    def test_main(self, tmpdir) -> None:
+        # arrange
+        select_path = os.path.join(tmpdir, "select")
+        processed_path = os.path.join(tmpdir, "processed")
+        subprocess.run(["mkdir", select_path])
+
+        data_2017 = "DS_VT_ESC_OCE,TP_PRES\n1,2\n"
+        data_2014 = "DS_VT_ESC_OCE,TP_PRES\n2,3\n"
+        data_2011 = "DS_VT_ESC_OCE,TP_PRES\n3,4\n"
+        data_2008 = "vt_esc_oce,tp_pres\n4,5\n"
+        data_2005 = "vt_esc_oce,tp_pres\n5,6\n"
+        output_data_2008 = "DS_VT_ESC_OCE,TP_PRES\n4,5\n"
+        output_data_2005 = "DS_VT_ESC_OCE,TP_PRES\n5,6\n"
+
+        years = [2017, 2014, 2011, 2008, 2005]
+        data = [data_2017, data_2014, data_2011, data_2008, data_2005]
+        output_data = [data_2017, data_2014, data_2011, output_data_2008,
+                       output_data_2005]
+
+        for year, string in zip(years, data):
+            file_name = os.path.join(select_path,
+                                     f"microdados_ciencia_computacao_{year}.csv")
+            with open(file_name, "w") as f:
+                f.write(string)
+
+        # execute
+        pre_process.main(select_path=select_path,
+                         processed_path=processed_path)
+
+        # assert
+        for year, string in zip(years, output_data):
+            file_name = os.path.join(processed_path,
+                                    f"microdados_processado_{year}.csv")
+            with open(file_name, "r") as f:
+                data = f.read()
+                assert data == string
+
+
+
+
